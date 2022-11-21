08fdfe5fc2d094b13cfc1781341763502ee4405c
shgur7236 <shgurtns7236@naver.com>
2022-11-21 22:59:08 +0900

    git init

---
+++ src/main/java/com/real/realoasis/RealoasisApplication.java
@@ -0,0 +1,13 @@
+package com.real.realoasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealoasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealoasisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/real/realoasis/RealoasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real.realoasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealoasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
