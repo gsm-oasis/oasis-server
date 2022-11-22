6a3e7e325a8f1d99f22ba1106f844d2f122a61ad
shgur7236 <shgurtns7236@naver.com>
2022-11-22 10:47:38 +0900

    UserRepository 생성, findUserByEmail 메서드 추가

---
+++ src/main/java/com/real/realoasis/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.real.realoasis.domain.user.repository;
+
+
+import com.real.realoasis.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,String> {
+    Optional<User> findUserByEmail(String email);
+}
