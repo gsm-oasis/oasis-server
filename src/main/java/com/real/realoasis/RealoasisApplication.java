4e0e9494f4dfdde8d3675c26178ee142e17c1ad8
shgur7236 <shgurtns7236@naver.com>
2022-11-22 14:00:46 +0900

    application에도 추가해야 사용 가능

--- src/main/java/com/real/realoasis/RealoasisApplication.java
+++ src/main/java/com/real/realoasis/RealoasisApplication.java
@@ -2,7 +2,9 @@ package com.real.realoasis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan(basePackages = {"com.real.*"})
 @SpringBootApplication
 public class RealoasisApplication {
 
