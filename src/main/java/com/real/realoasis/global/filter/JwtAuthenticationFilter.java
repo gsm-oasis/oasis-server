27b4b9f78efc68b3c121db1ed3d7e01fd98b87fe
shgur7236 <shgurtns7236@naver.com>
2022-11-29 21:10:37 +0900

    @Component 사용하여 빈 등록

--- src/main/java/com/real/realoasis/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/real/realoasis/global/filter/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.real.realoasis.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
