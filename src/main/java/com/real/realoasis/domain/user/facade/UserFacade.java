73c08b31be5894982a5859f5863dcca1fc294960
shgur7236 <shgurtns7236@naver.com>
2022-11-29 14:14:48 +0900

    facade 패턴 사용

---
+++ src/main/java/com/real/realoasis/domain/user/facade/UserFacade.java
@@ -0,0 +1,48 @@
+package com.real.realoasis.domain.user.facade;
+
+import com.real.realoasis.domain.auth.presentation.dto.request.SignUpRequest;
+import com.real.realoasis.domain.user.entity.User;
+import com.real.realoasis.domain.user.exception.PasswordNotMatchException;
+import com.real.realoasis.domain.user.exception.UserNotFoundException;
+import com.real.realoasis.domain.user.repository.UserRepository;
+import com.real.realoasis.global.error.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class UserFacade {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    public boolean existsById(String id) {
+        return userRepository.existsById(id);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void saveUser(SignUpRequest signUpRequest) {
+        userRepository.save(signUpRequest.toEntity(passwordEncoder.encode(signUpRequest.getPassword())));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email).orElseThrow(() -> new UserNotFoundException(ErrorCode.USER_NOT_FOUND_EXCEPTION));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void checkPassword(User user, String password) {
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new PasswordNotMatchException(ErrorCode.PASSWORD_NOT_MATCH_EXCEPTION);
+        }
+    }
+
+    public User currentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return findUserByEmail(email);
+    }
+
+}
