fc4a8500d9f5b72325c5e3c06ceea263f82d3ea4
shgur7236 <shgurtns7236@naver.com>
2022-11-29 14:24:55 +0900

    생성자 삭제, @CollectionTable추가

--- src/main/java/com/real/realoasis/domain/user/entity/User.java
+++ src/main/java/com/real/realoasis/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.real.realoasis.domain.user.entity;
 
+import com.real.realoasis.domain.user.type.Role;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,23 +23,17 @@ public class User {
     @NotNull
     private String password;
 
-    private String name;
+    private String nickName;
     @Column(name = "refresh_token")
     private String refreshToken;
 
     @ElementCollection(fetch = FetchType.EAGER) // roles 컬렉션
+    @CollectionTable(name = "role", joinColumns = @JoinColumn(name = "email"))
     @Builder.Default
-    private List<String> roles = new ArrayList<>();
+    private List<Role> roles = new ArrayList<>();
 
     public void updateRefreshToken(String refreshToken){
         this.refreshToken = refreshToken;
-    }
-
-    public User(String email, String password, String refreshToken){
-        this.email = email;
-        this.password = password;
-        this.refreshToken = refreshToken;
-        this.name = null;
     }
 
 
