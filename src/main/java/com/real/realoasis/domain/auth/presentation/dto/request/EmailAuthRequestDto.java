bb832f70c0f2d5f4b08524f16e68ad14eed911ba
shgur7236 <shgurtns7236@naver.com>
2022-11-30 23:32:28 +0900

    Email req

---
+++ src/main/java/com/real/realoasis/domain/auth/presentation/dto/request/EmailAuthRequestDto.java
@@ -0,0 +1,15 @@
+package com.real.realoasis.domain.auth.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailAuthRequestDto {
+    private String email;
+
+}
