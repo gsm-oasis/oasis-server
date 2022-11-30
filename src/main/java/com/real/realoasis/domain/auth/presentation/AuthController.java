f9bcebe2db7f92c80b9cdb925ce3457e34580034
shgur7236 <shgurtns7236@naver.com>
2022-11-30 23:32:45 +0900

    이메일 인증

--- src/main/java/com/real/realoasis/domain/auth/presentation/AuthController.java
+++ src/main/java/com/real/realoasis/domain/auth/presentation/AuthController.java
@@ -1,9 +1,11 @@
 package com.real.realoasis.domain.auth.presentation;
 
+import com.real.realoasis.domain.auth.presentation.dto.request.EmailAuthRequestDto;
 import com.real.realoasis.domain.auth.presentation.dto.request.LoginRequest;
 import com.real.realoasis.domain.auth.presentation.dto.request.SignUpRequest;
 import com.real.realoasis.domain.auth.presentation.dto.response.LoginResponse;
 import com.real.realoasis.domain.auth.presentation.dto.response.ReissueTokenResponse;
+import com.real.realoasis.domain.auth.service.EmailService;
 import com.real.realoasis.domain.auth.service.LoginService;
 import com.real.realoasis.domain.auth.service.SignUpService;
 import com.real.realoasis.domain.auth.service.ReissueTokenService;
@@ -12,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("auth")
@@ -21,12 +25,19 @@ public class AuthController {
     private final LoginService loginService;
     private final SignUpService signUpService;
     private final ReissueTokenService reissueTokenService;
+    private final EmailService emailService;
 
     // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest signupRequest) {
         signUpService.signUp(signupRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    // 이메일 인증
+    @PostMapping("/signup/mailconfirm")
+    public String mailConfirm(@RequestBody EmailAuthRequestDto emailDto) throws MessagingException, UnsupportedEncodingException {
+        return emailService.sendEmail(emailDto.getEmail());
     }
 
     // 로그인
@@ -40,4 +51,6 @@ public class AuthController {
     public ResponseEntity<ReissueTokenResponse> refresh(@RequestHeader("Refresh") String refreshToken){
         return new ResponseEntity<>(reissueTokenService.reissue(refreshToken), HttpStatus.OK);
     }
+
+
 }
