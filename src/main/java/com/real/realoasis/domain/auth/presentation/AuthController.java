013e1dae84231fb5a1b6a40688ae3c444ff4394f
shgur7236 <shgurtns7236@naver.com>
2022-11-30 13:39:20 +0900

    토큰 재발급 부분 controller

--- src/main/java/com/real/realoasis/domain/auth/presentation/AuthController.java
+++ src/main/java/com/real/realoasis/domain/auth/presentation/AuthController.java
@@ -3,16 +3,14 @@ package com.real.realoasis.domain.auth.presentation;
 import com.real.realoasis.domain.auth.presentation.dto.request.LoginRequest;
 import com.real.realoasis.domain.auth.presentation.dto.request.SignUpRequest;
 import com.real.realoasis.domain.auth.presentation.dto.response.LoginResponse;
+import com.real.realoasis.domain.auth.presentation.dto.response.ReissueTokenResponse;
 import com.real.realoasis.domain.auth.service.LoginService;
 import com.real.realoasis.domain.auth.service.SignUpService;
 import com.real.realoasis.domain.auth.service.ReissueTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -35,5 +33,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         return new ResponseEntity<>(loginService.login(loginRequest), HttpStatus.OK);
+    }
+
+    // 토큰 재발급
+    @PutMapping("/refresh")
+    public ResponseEntity<ReissueTokenResponse> refresh(@RequestHeader("Refresh") String refreshToken){
+        return new ResponseEntity<>(reissueTokenService.reissue(refreshToken), HttpStatus.OK);
     }
 }
