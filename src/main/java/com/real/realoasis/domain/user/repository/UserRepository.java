1264f330d5f10f6f9cbf707195038fc957550d72
shgur7236 <shgurtns7236@naver.com>
2022-12-01 18:13:14 +0900

    findUserByEmail 생성

--- src/main/java/com/real/realoasis/domain/user/repository/UserRepository.java
+++ src/main/java/com/real/realoasis/domain/user/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
     Optional<User> findUserById(String id);
+    User findUserByEmail(String email);
 }
