6c176c4291536d1d9bdb2af4b4617d125ffd4ba6
shgur7236 <shgurtns7236@naver.com>
2022-12-06 00:52:54 +0900

    커플연결 추가

--- src/main/java/com/real/realoasis/domain/user/presentation/UserController.java
+++ src/main/java/com/real/realoasis/domain/user/presentation/UserController.java
@@ -1,7 +1,7 @@
 package com.real.realoasis.domain.user.presentation;
 
-import com.real.realoasis.domain.auth.service.EmailService;
-import com.real.realoasis.domain.auth.service.SearchPWService;
+import com.real.realoasis.domain.user.presentation.request.ConnectCoupleRequest;
+import com.real.realoasis.domain.user.service.ConnectCoupleService;
 import com.real.realoasis.domain.user.service.WithdrawalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,13 +10,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
     private final WithdrawalService withdrawalService;
+    private final ConnectCoupleService connectCoupleService;
+
     // 회원탈퇴
     @DeleteMapping("/withdrawal")
     public ResponseEntity<Void> withdrawal(){
         withdrawalService.withdrawal();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    // 커플연결
+    @PostMapping("/connect/couple")
+    public ResponseEntity<Void> connectCouple(@RequestBody ConnectCoupleRequest connectCoupleRequest){
+        connectCoupleService.connectCouple(connectCoupleRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
