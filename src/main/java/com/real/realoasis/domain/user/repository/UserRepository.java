f95a2cbceac8afc6fc1f0b23fea8d0d5f2209589
shgur7236 <shgurtns7236@naver.com>
2022-12-06 00:54:07 +0900

    findUserByCode 추가

--- src/main/java/com/real/realoasis/domain/user/repository/UserRepository.java
+++ src/main/java/com/real/realoasis/domain/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,String> {
     Optional<User> findUserById(String id);
     User findUserByEmail(String email);
+
+    Optional<User> findUserByCode(String code);
 }
