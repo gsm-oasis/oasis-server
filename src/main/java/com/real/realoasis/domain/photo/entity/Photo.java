37205770dc9cd66674367c7e282392d4fd43e136
shgur7236 <shgurtns7236@naver.com>
2022-12-07 01:56:27 +0900

    Photo(file) entity

---
+++ src/main/java/com/real/realoasis/domain/photo/entity/Photo.java
@@ -0,0 +1,32 @@
+package com.real.realoasis.domain.photo.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String origFileName; // 파일 원본명
+
+    @Column(nullable = false)
+    private String filePath; // 파일 저장 경로
+
+    private Long fileSize;
+
+    @Builder
+    public Photo(String origFileName, String filePath, Long fileSize) {
+        this.origFileName = origFileName;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+    }
+}
