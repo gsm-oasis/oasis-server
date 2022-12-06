10ee34ea9c2afcf8575956b38fd1440ebc2ebf76
shgur7236 <shgurtns7236@naver.com>
2022-12-07 01:55:46 +0900

    일기 저장

--- src/main/java/com/real/realoasis/domain/diary/facade/DiaryFacade.java
+++ src/main/java/com/real/realoasis/domain/diary/facade/DiaryFacade.java
@@ -3,8 +3,7 @@ package com.real.realoasis.domain.diary.facade;
 import com.real.realoasis.domain.diary.entity.Diary;
 import com.real.realoasis.domain.diary.exception.DiaryNotFoundException;
 import com.real.realoasis.domain.diary.repository.DiaryRepository;
-import com.real.realoasis.domain.user.entity.User;
-import com.real.realoasis.domain.user.exception.UserNotFoundException;
+import com.real.realoasis.domain.photo.entity.Photo;
 import com.real.realoasis.global.error.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -23,6 +22,11 @@ public class DiaryFacade {
     @Transactional(rollbackFor = Exception.class)
     public Diary findDiaryById(Long id) {
         return diaryRepository.findDiaryById(id).orElseThrow(() -> new DiaryNotFoundException(ErrorCode.DIARY_NOT_FOUND_EXCEPTION));
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void saveDairy(Diary diary) {
+        diaryRepository.save(diary).createDate();
     }
 
 
