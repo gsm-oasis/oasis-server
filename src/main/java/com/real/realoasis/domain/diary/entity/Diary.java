a0b6afd60ad09c1c0e0b1310c51a05ab9049011e
shgur7236 <shgurtns7236@naver.com>
2022-12-06 14:15:08 +0900

    USER랑 조인, 엔티티 생성

---
+++ src/main/java/com/real/realoasis/domain/diary/entity/Diary.java
@@ -0,0 +1,38 @@
+package com.real.realoasis.domain.diary.entity;
+
+import com.real.realoasis.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Diary {
+    @Id
+    @Column(name = "diary_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long diaryId;
+
+    private String content;
+
+    private String image;
+
+    private String mood;
+
+    private String date;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete( action = OnDeleteAction.CASCADE)
+    private User user;
+}
