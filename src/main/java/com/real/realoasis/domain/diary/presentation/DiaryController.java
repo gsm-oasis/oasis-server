6b9bff80c3adebd47ea2315bc7b3e4f089fd0bd0
shgur7236 <shgurtns7236@naver.com>
2022-12-07 12:28:56 +0900

    일기 수정 controller

--- src/main/java/com/real/realoasis/domain/diary/presentation/DiaryController.java
+++ src/main/java/com/real/realoasis/domain/diary/presentation/DiaryController.java
@@ -1,7 +1,9 @@
 package com.real.realoasis.domain.diary.presentation;
 
 import com.real.realoasis.domain.diary.presentation.dto.request.CreateDiaryRequest;
+import com.real.realoasis.domain.diary.presentation.dto.request.EditDiaryRequest;
 import com.real.realoasis.domain.diary.service.CreateDiaryService;
+import com.real.realoasis.domain.diary.service.EditDiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +17,23 @@ import java.util.List;
 @RequestMapping("/diary")
 public class DiaryController {
     private final CreateDiaryService createDiaryService;
+    private final EditDiaryService editDiaryService;
 
     @PostMapping("/create")
-    public ResponseEntity<Void> createDairy(
+    public ResponseEntity<Void> createDiary(
             @RequestPart(value = "file", required = false)List<MultipartFile> files,
             @RequestPart(value = "req") CreateDiaryRequest createDiaryRequest) throws Exception {
         createDiaryService.createDiary(createDiaryRequest, files);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @PutMapping("/edit/{diaryId}")
+    public ResponseEntity<Void> editDiary(
+            @PathVariable Long diaryId,
+            @RequestPart(value = "file", required = false)List<MultipartFile> files,
+            @RequestPart(value = "req") EditDiaryRequest editDiaryRequest) throws Exception {
+        editDiaryService.editDiary(diaryId, editDiaryRequest, files);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
