8a227acc9e8323056a0775d80500f1a89fc8d30d
shgur7236 <shgurtns7236@naver.com>
2022-12-08 08:00:23 +0900

    일기 list res

---
+++ src/main/java/com/real/realoasis/domain/diary/presentation/dto/response/ListDiaryPageResponse.java
@@ -0,0 +1,21 @@
+package com.real.realoasis.domain.diary.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListDiaryPageResponse {
+    private Long diaryId;
+    private String content;
+    private String mood;
+    private String title;
+    private String writer;
+    private LocalDateTime createDate;
+}
