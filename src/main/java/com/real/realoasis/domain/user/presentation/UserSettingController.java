f4d2e25c99cdc1fdc9df3958d2ac0bcb73c2129c
shgur7236 <shgurtns7236@naver.com>
2022-12-09 00:26:11 +0900

    질문시간 변경 controller

--- src/main/java/com/real/realoasis/domain/user/presentation/UserSettingController.java
+++ src/main/java/com/real/realoasis/domain/user/presentation/UserSettingController.java
@@ -2,10 +2,12 @@ package com.real.realoasis.domain.user.presentation;
 
 import com.real.realoasis.domain.user.presentation.dto.request.NicknameChangeRequest;
 import com.real.realoasis.domain.user.presentation.dto.request.PasswordChangeRequest;
+import com.real.realoasis.domain.user.presentation.dto.request.QuestionTimeChangeRequest;
 import com.real.realoasis.domain.user.presentation.dto.response.SettingResponse;
 import com.real.realoasis.domain.user.service.GetSettingService;
 import com.real.realoasis.domain.user.service.NicknameChangeService;
 import com.real.realoasis.domain.user.service.PasswordChangeService;
+import com.real.realoasis.domain.user.service.QuestionTimeChangeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ public class UserSettingController {
     private final GetSettingService getSettingService;
     private final NicknameChangeService nicknameChangeService;
     private final PasswordChangeService passwordChangeService;
+    private final QuestionTimeChangeService questionTimeChangeService;
 
     // 설정 메인 페이지
     @GetMapping("/")
@@ -36,6 +39,13 @@ public class UserSettingController {
     @PutMapping("/change/password")
     public ResponseEntity<Void> changePassword(@RequestBody PasswordChangeRequest passwordChangeRequest){
         passwordChangeService.passwordChange(passwordChangeRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 질문시간 변경 페이지
+    @PutMapping("/change/questiontime")
+    public ResponseEntity<Void> changeQuestionTime(@RequestBody QuestionTimeChangeRequest questionTimeChangeRequest){
+        questionTimeChangeService.questionTimeChange(questionTimeChangeRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
