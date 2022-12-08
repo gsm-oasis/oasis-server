24655aaf52c96f8c06a07392cfe4a4c8f4da6404
shgur7236 <shgurtns7236@naver.com>
2022-12-09 00:25:24 +0900

    질문시간 변경 service

---
+++ src/main/java/com/real/realoasis/domain/user/service/QuestionTimeChangeService.java
@@ -0,0 +1,8 @@
+package com.real.realoasis.domain.user.service;
+
+
+import com.real.realoasis.domain.user.presentation.dto.request.QuestionTimeChangeRequest;
+
+public interface QuestionTimeChangeService {
+    void questionTimeChange(QuestionTimeChangeRequest questionTimeChangeRequest);
+}
