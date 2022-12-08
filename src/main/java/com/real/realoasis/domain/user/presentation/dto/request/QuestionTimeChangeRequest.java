807eaab38a0c412fbd38b902359cce8db12a8b26
shgur7236 <shgurtns7236@naver.com>
2022-12-09 00:24:18 +0900

    질문시간 변경 request

---
+++ src/main/java/com/real/realoasis/domain/user/presentation/dto/request/QuestionTimeChangeRequest.java
@@ -0,0 +1,14 @@
+package com.real.realoasis.domain.user.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionTimeChangeRequest {
+    private String questionTime;
+}
