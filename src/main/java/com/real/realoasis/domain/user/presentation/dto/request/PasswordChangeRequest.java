870a99812cf4c611fcdcb60d835c3cee379488fd
shgur7236 <shgurtns7236@naver.com>
2022-12-08 23:48:54 +0900

    비밀번호 변경 request

---
+++ src/main/java/com/real/realoasis/domain/user/presentation/dto/request/PasswordChangeRequest.java
@@ -0,0 +1,14 @@
+package com.real.realoasis.domain.user.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordChangeRequest {
+    private String password;
+}
