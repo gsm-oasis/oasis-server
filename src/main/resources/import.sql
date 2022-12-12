24b54cb43e6066a78f6192beaea605dd1c0f49a6
shgur7236 <shgurtns7236@naver.com>
2022-12-13 00:24:26 +0900

    실행했을 때 db 넣어줘야 할 데이터

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO user(user_id, anniversary_time, code, dating_date, email, nick_name, password, question_time, today) values ('adminId', 5, 'o4KlNO0H', 0, 'shgur7236@naver.com','admin','$2a$10$5gLLAwYnrS614Ay3bVc4E.vnJZ/0a0blCNkm1m9p5SyVKJoQFY8/K','12:00','20221212');
+INSERT INTO question(content,user_id) values ('상대방의 첫인상을 말해주세요','adminId');
+INSERT INTO question(content,user_id) values ('상대방의 장점 4가지만 말해주세요','adminId');
+INSERT INTO question(content,user_id) values ('자신의 약점을 말해보세요','adminId');
