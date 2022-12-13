b7eddb350e085725a430065ae144757116b4c2b0
shgur7236 <shgurtns7236@naver.com>
2022-12-14 00:18:51 +0900

    파일 삭제

--- src/main/java/com/real/realoasis/domain/question/service/CreateAnswerService.java
+++
@@ -1,8 +0,0 @@
-package com.real.realoasis.domain.question.service;
-
-
-import com.real.realoasis.domain.question.presentation.dto.request.QuestionAnswerRequest;
-
-public interface CreateAnswerService {
-    void createAnswer(QuestionAnswerRequest questionAnswerRequest, Long questionId);
-}
