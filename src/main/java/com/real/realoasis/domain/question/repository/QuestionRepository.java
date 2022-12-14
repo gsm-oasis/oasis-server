8bef3b0b3374e4576ad689d89387a4c787d5a627
shgur7236 <shgurtns7236@naver.com>
2022-12-14 20:33:41 +0900

    QuestionRepository 생성

---
+++ src/main/java/com/real/realoasis/domain/question/repository/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.real.realoasis.domain.question.repository;
+
+import com.real.realoasis.domain.question.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findQuestionById(Long id);
+}
