04e3f39191567e8b9ee75553e2ed859ae62b5183
shgur7236 <shgurtns7236@naver.com>
2022-12-15 15:46:37 +0900

    질문답변 메인페이지에 사용할 메서드 구현

--- src/main/java/com/real/realoasis/domain/questionAnswer/repository/QuestionAnswerRepository.java
+++ src/main/java/com/real/realoasis/domain/questionAnswer/repository/QuestionAnswerRepository.java
@@ -3,8 +3,8 @@ package com.real.realoasis.domain.questionAnswer.repository;
 import com.real.realoasis.domain.questionAnswer.entity.QuestionAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface QuestionAnswerRepository extends JpaRepository<QuestionAnswer, Long> {
-    Optional<QuestionAnswer> findQuestionById(Long questionId);
+
+    QuestionAnswer findQuestionAnswerByQuestionIdAndUserId(Long questionId, String userId);
 }
