32c0b48f321ff63e1bc5a3277f9fd11dafecce89
shgur7236 <shgurtns7236@naver.com>
2022-12-15 20:10:53 +0900

    .gitignore

--- src/main/java/com/real/realoasis/domain/user/service/Impl/GetMainPageServiceImpl.java
+++ src/main/java/com/real/realoasis/domain/user/service/Impl/GetMainPageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.real.realoasis.domain.user.service.Impl;
 
 import com.real.realoasis.domain.diary.service.DiaryListPageService;
+import com.real.realoasis.domain.question.entity.Question;
+import com.real.realoasis.domain.questionAnswer.facade.QuestionAnswerFacade;
 import com.real.realoasis.domain.user.entity.User;
 import com.real.realoasis.domain.user.facade.UserFacade;
 import com.real.realoasis.domain.user.presentation.dto.response.MainPageResponse;
@@ -18,6 +20,7 @@ import java.time.format.DateTimeFormatter;
 public class GetMainPageServiceImpl implements GetMainPageService {
     private final UserFacade userFacade;
     private final DiaryListPageService diaryListPageService;
+    private final QuestionAnswerFacade questionAnswerFacade;
 
     @Override
     public MainPageResponse getMainPage() {
@@ -33,12 +36,14 @@ public class GetMainPageServiceImpl implements GetMainPageService {
         int datingDate = diff.getYears() + diff.getMonths() + diff.getDays();
         currentUser.updateDatingDate(datingDate);
 
+        Question question = questionAnswerFacade.findQuestionByQuestionId((long) datingDate);
+
         return MainPageResponse.builder()
                 .userName(currentUser.getNickname())
                 .coupleName(coupleUser.getNickname())
                 .datingDate(datingDate)
-                .questionId(1L)
-                .content("아직안만듬")
+                .questionId(question.getId())
+                .content(question.getContent())
                 .diaryListPageResponse(diaryListPageService.getList())
                 .build();
     }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,15 @@
-INSERT INTO user(user_id, anniversary_time, code, dating_date, email, nick_name, password, question_time, today) values ('adminId', 5, 'o4KlNO0H', 0, 'shgur7236@naver.com','admin','$2a$10$5gLLAwYnrS614Ay3bVc4E.vnJZ/0a0blCNkm1m9p5SyVKJoQFY8/K','12:00','20221212');
-INSERT INTO question(content,user_id) values ('상대방의 첫인상을 말해주세요','adminId');
-INSERT INTO question(content,user_id) values ('상대방의 장점 4가지만 말해주세요','adminId');
-INSERT INTO question(content,user_id) values ('자신의 약점을 말해보세요','adminId');
+INSERT INTO user(user_id, anniversary_time, code, dating_date, email, nickname, password, question_time, today) values ('adminId', 5, 'o4KlNO0H', 0, 'shgur7236@naver.com','admin','$2a$10$5gLLAwYnrS614Ay3bVc4E.vnJZ/0a0blCNkm1m9p5SyVKJoQFY8/K','12:00','20221212');
+
+INSERT INTO diary(title,content,mood,writer, user_id, create_date) values ('제목','일기 내용~~~~~','행복','노혁','adminId','20221215');
+
+# INSERT INTO photo()
+#
+# INSERT INTO diary_photo(diary_diary_id, photo_file_id) VALUES (1,1);
+
+INSERT INTO question(id,content) values (1,'상대방의 첫인상을 말해주세요');
+INSERT INTO question(id,content) values (2,'상대방의 장점 4가지만 말해주세요');
+INSERT INTO question(id,content) values (3,'자신의 약점을 말해보세요');
+INSERT INTO question(id,content) values (4,'상대방의 닮은 동물은 무엇이 있을까요?');
+INSERT INTO question(id,content) values (5,'상대방의 닮은 사람은?');
+
+
