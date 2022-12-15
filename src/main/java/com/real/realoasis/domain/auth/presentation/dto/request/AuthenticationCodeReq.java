97440b5aa7684d2c6d45710ab6ca7ce95aec2293
shgur7236 <shgurtns7236@naver.com>
2022-12-15 10:47:25 +0900

    인증코드 할 때 AuthenticationCode라고 변수명을 사용했는데 너무 길어서 짧게 code로 수정

--- src/main/java/com/real/realoasis/domain/auth/presentation/dto/request/AuthenticationCodeReq.java
+++ src/main/java/com/real/realoasis/domain/auth/presentation/dto/request/AuthenticationCodeReq.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationCodeReq {
-    private String authenticationCode;
-
+    private String code;
 }
