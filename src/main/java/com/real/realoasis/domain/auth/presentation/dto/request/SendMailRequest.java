1878e17086c920160f36e3d4b995f96603611edd
shgur7236 <shgurtns7236@naver.com>
2022-12-15 10:49:25 +0900

    sendmail request

---
+++ src/main/java/com/real/realoasis/domain/auth/presentation/dto/request/SendMailRequest.java
@@ -0,0 +1,13 @@
+package com.real.realoasis.domain.auth.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+public class SendMailRequest {
+    private String email;
+}
