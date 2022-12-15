cdd17d933c2108ffa9c3fb6f4f853f9e58fa4705
shgur7236 <shgurtns7236@naver.com>
2022-12-15 15:47:02 +0900

    질문답변 메인페이지 controller

--- src/main/java/com/real/realoasis/domain/questionAnswer/presentation/QuestionAnswerController.java
+++ src/main/java/com/real/realoasis/domain/questionAnswer/presentation/QuestionAnswerController.java
@@ -1,7 +1,9 @@
 package com.real.realoasis.domain.questionAnswer.presentation;
 
-import com.real.realoasis.domain.questionAnswer.presentation.dto.request.QuestionAnswerRequest;
+import com.real.realoasis.domain.questionAnswer.presentation.dto.request.QuestionAnswerWriteRequest;
+import com.real.realoasis.domain.questionAnswer.presentation.dto.response.QuestionAnswerResponse;
 import com.real.realoasis.domain.questionAnswer.service.CreateQuestionAnswerService;
+import com.real.realoasis.domain.questionAnswer.service.GetQuestionAnswerMainpageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/question")
 public class QuestionAnswerController {
     private final CreateQuestionAnswerService createQuestionAnswerService;
+    private final GetQuestionAnswerMainpageService getQuestionAnswerMainpageService;
 
     @PostMapping("/answer/{questionId}")
-    public ResponseEntity<Void> createAnswer(@PathVariable Long questionId, @RequestBody QuestionAnswerRequest questionAnswerRequest){
+    public ResponseEntity<Void> createAnswer(@PathVariable Long questionId, @RequestBody QuestionAnswerWriteRequest questionAnswerRequest){
         createQuestionAnswerService.createQuestionAnswer(questionAnswerRequest, questionId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping("/{questionId}")
+    public ResponseEntity<QuestionAnswerResponse> getMainpage(@PathVariable Long questionId){
+        return new ResponseEntity<>(getQuestionAnswerMainpageService.getMainpage(questionId), HttpStatus.OK);
+    }
+
 }
