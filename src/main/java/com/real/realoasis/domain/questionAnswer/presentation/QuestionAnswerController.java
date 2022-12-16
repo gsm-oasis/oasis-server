18f7568617c34def1167b21bc1154fe59bdb1703
shgur7236 <shgurtns7236@naver.com>
2022-12-16 19:09:39 +0900

    질문대답 목록 controller

--- src/main/java/com/real/realoasis/domain/questionAnswer/presentation/QuestionAnswerController.java
+++ src/main/java/com/real/realoasis/domain/questionAnswer/presentation/QuestionAnswerController.java
@@ -1,13 +1,17 @@
 package com.real.realoasis.domain.questionAnswer.presentation;
 
 import com.real.realoasis.domain.questionAnswer.presentation.dto.request.QuestionAnswerWriteRequest;
+import com.real.realoasis.domain.questionAnswer.presentation.dto.response.QuestionAnswerListResponse;
 import com.real.realoasis.domain.questionAnswer.presentation.dto.response.QuestionAnswerResponse;
 import com.real.realoasis.domain.questionAnswer.service.CreateQuestionAnswerService;
+import com.real.realoasis.domain.questionAnswer.service.GetQuestionAnswerListService;
 import com.real.realoasis.domain.questionAnswer.service.GetQuestionAnswerMainpageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class QuestionAnswerController {
     private final CreateQuestionAnswerService createQuestionAnswerService;
     private final GetQuestionAnswerMainpageService getQuestionAnswerMainpageService;
+    private final GetQuestionAnswerListService getQuestionAnswerListService;
 
     @PostMapping("/answer/{questionId}")
     public ResponseEntity<Void> createAnswer(@PathVariable Long questionId, @RequestBody QuestionAnswerWriteRequest questionAnswerRequest){
@@ -25,6 +30,11 @@ public class QuestionAnswerController {
     @GetMapping("/{questionId}")
     public ResponseEntity<QuestionAnswerResponse> getMainpage(@PathVariable Long questionId){
         return new ResponseEntity<>(getQuestionAnswerMainpageService.getMainpage(questionId), HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Stream<QuestionAnswerListResponse>> getListPage(){
+        return new ResponseEntity<>(getQuestionAnswerListService.getList(), HttpStatus.OK);
     }
 
 }
