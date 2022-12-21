34e5b453ae3cc2d4b1eda19145e19edbe75919c4
shgur7236 <shgurtns7236@naver.com>
2022-12-21 20:16:22 +0900

    CustomAuthenticationEntryPoint 생성

---
+++ src/main/java/com/real/realoasis/global/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.real.realoasis.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.real.realoasis.global.error.response.ErrorResponse;
+import com.real.realoasis.global.error.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        String errorResponseJson = objectMapper.writeValueAsString(
+                new ErrorResponse(errorCode.getStatus(), errorCode.getMessage())
+        );
+        response.setStatus(errorCode.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(errorResponseJson);
+
+    }
+}
