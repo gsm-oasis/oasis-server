309211f02dbfaddee7982d4a12e64eb2fa311aa0
shgur7236 <shgurtns7236@naver.com>
2022-12-21 20:16:34 +0900

    400 ~ 500 순서대로 나열

--- src/main/java/com/real/realoasis/global/error/type/ErrorCode.java
+++ src/main/java/com/real/realoasis/global/error/type/ErrorCode.java
@@ -6,26 +6,19 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
-    //user
-    USER_NOT_FOUND_EXCEPTION("USER 를 찾을 수 없습니다.",404),
     PASSWORD_NOT_MATCH_EXCEPTION("비밀번호가 일치하지 않습니다.", 400),
-    DUPLICATE_ID_EXCEPTION("이미 존재하는 아이디입니다.",409),
-
-    // mail
-    INVALID_AUTH_CODE_EXCEPTION("옳지 않은 인증 코드입니다.", 404),
-    // token
+    UNAUTHORIZED("UNAUTHORIZED", 401),
     EXPIRATION_TOKEN_EXCEPTION("만료된 토큰입니다.", 401),
     INVALID_TOKEN_EXCEPTION("유효하지 않은 토큰입니다.", 401),
-
-
-    // question
+    USER_NOT_FOUND_EXCEPTION("USER 를 찾을 수 없습니다.",404),
+    INVALID_AUTH_CODE_EXCEPTION("옳지 않은 인증 코드입니다.", 404),
     QUESTION_NOT_FOUND_EXCEPTION("질문을 찾을 수 없습니다.",404),
 
-    // question answer
     QUESTION_ANSWER_NOT_FOUND_EXCEPTION("답변을 찾을 수 없습니다.", 404),
-    // diary
-    DIARY_NOT_FOUND_EXCEPTION("일기를 찾을 수 없습니다.", 404);
+    DIARY_NOT_FOUND_EXCEPTION("일기를 찾을 수 없습니다.", 404),
+    DUPLICATE_ID_EXCEPTION("이미 존재하는 아이디입니다.",409);
+
+
 
     private final String message;
     private final int status;
