6a502e700b6b608ded4d0c6d72f472e28de9bc1e
shgur7236 <shgurtns7236@naver.com>
2022-12-29 23:19:29 +0900

    일기수정 수정

--- src/main/java/com/real/realoasis/domain/diary/service/DiaryEditService.java
+++ src/main/java/com/real/realoasis/domain/diary/service/DiaryEditService.java
@@ -2,10 +2,7 @@ package com.real.realoasis.domain.diary.service;
 
 
 import com.real.realoasis.domain.diary.presentation.dto.request.DiaryEditRequest;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 public interface DiaryEditService {
-    void editDiary(Long diaryId, DiaryEditRequest editDiaryRequest, List<MultipartFile> multipartFileList) throws Exception;
+    void editDiary(Long diaryId, DiaryEditRequest editDiaryRequest);
 }
--- src/main/java/com/real/realoasis/domain/diary/service/Impl/DiaryEditServiceImpl.java
+++ src/main/java/com/real/realoasis/domain/diary/service/Impl/DiaryEditServiceImpl.java
@@ -4,35 +4,18 @@ import com.real.realoasis.domain.diary.entity.Diary;
 import com.real.realoasis.domain.diary.facade.DiaryFacade;
 import com.real.realoasis.domain.diary.presentation.dto.request.DiaryEditRequest;
 import com.real.realoasis.domain.diary.service.DiaryEditService;
-import com.real.realoasis.domain.image.data.entity.Image;
-import com.real.realoasis.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryEditServiceImpl implements DiaryEditService {
     private final DiaryFacade diaryFacade;
-    private final ImageService imageService;
 
     @Override
-    public void editDiary(Long diaryId, DiaryEditRequest editDiaryRequest, List<MultipartFile> files) throws Exception {
-        // 수정할 일기 객체 찾기
+    public void editDiary(Long diaryId, DiaryEditRequest editDiaryRequest) {
         Diary editDiary = diaryFacade.findDiaryById(diaryId);
-        List<String> imgUrlList = imageService.upload(files);
-
-        if(!imgUrlList.isEmpty()) {
-            List<Image> list = new ArrayList<>();
-            for(String imgUrl : imgUrlList) {
-                Image image = new Image(imgUrl);
-                list.add(image);
-            }
-            editDiary.updateImages(list);
-        }
         editDiary.update(editDiaryRequest.getTitle(), editDiaryRequest.getContent(), editDiaryRequest.getMood());
         diaryFacade.saveDiary(editDiary);
     }
