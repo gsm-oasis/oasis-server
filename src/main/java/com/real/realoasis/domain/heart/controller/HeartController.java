147385ca553584629d985c09174f617680454702
shgur7236 <shgurtns7236@naver.com>
2023-01-10 17:24:19 +0900

    :sparkles: HeartController 구현

--- src/main/java/com/real/realoasis/domain/heart/controller/HeartController.java
+++ src/main/java/com/real/realoasis/domain/heart/controller/HeartController.java
@@ -1,6 +1,11 @@
 package com.real.realoasis.domain.heart.controller;
 
+import com.real.realoasis.domain.heart.data.response.HeartResponse;
+import com.real.realoasis.domain.heart.service.HeartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/heart")
 public class HeartController {
+    private final HeartService heartService;
+
+    @GetMapping
+    public ResponseEntity<HeartResponse> getHeart(){
+        HeartResponse heartResponse = heartService.getHeart();
+        return new ResponseEntity<>(heartResponse, HttpStatus.OK);
+    }
 }
