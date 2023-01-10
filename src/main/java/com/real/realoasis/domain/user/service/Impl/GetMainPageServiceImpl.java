d3a1db28d054f6bd0ba0a9f2d074de812c8905f0
shgur7236 <shgurtns7236@naver.com>
2023-01-10 17:25:14 +0900

    :recycle: heartUtil을 사용하여 하트레벨 등록

--- src/main/java/com/real/realoasis/domain/user/service/Impl/GetMainPageServiceImpl.java
+++ src/main/java/com/real/realoasis/domain/user/service/Impl/GetMainPageServiceImpl.java
@@ -1,6 +1,7 @@
 package com.real.realoasis.domain.user.service.Impl;
 
 import com.real.realoasis.domain.diary.service.DiaryService;
+import com.real.realoasis.domain.heart.util.HeartUtil;
 import com.real.realoasis.domain.question.entity.Question;
 import com.real.realoasis.domain.questionAnswer.facade.QuestionAnswerFacade;
 import com.real.realoasis.domain.user.entity.User;
@@ -9,6 +10,7 @@ import com.real.realoasis.domain.user.presentation.dto.response.MainPageResponse
 import com.real.realoasis.domain.user.service.GetMainPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,7 +23,9 @@ public class GetMainPageServiceImpl implements GetMainPageService {
     private final UserFacade userFacade;
     private final DiaryService diaryService;
     private final QuestionAnswerFacade questionAnswerFacade;
+    private final HeartUtil heartUtil;
 
+    @Transactional
     @Override
     public MainPageResponse getMainPage() {
         User currentUser = userFacade.currentUser();
@@ -35,6 +39,9 @@ public class GetMainPageServiceImpl implements GetMainPageService {
         LocalDate todayToLocalDate = LocalDate.parse(currentUser.getToday(), dateFormat);
 
         long datingDate = ChronoUnit.DAYS.between(firstDayToLocalDate, todayToLocalDate);
+
+        currentUser.updateDatingDate(datingDate);
+        heartUtil.heartLevel(currentUser);
 
         Question question = questionAnswerFacade.findQuestionByQuestionId(datingDate - currentUser.getDatingDate());
 
