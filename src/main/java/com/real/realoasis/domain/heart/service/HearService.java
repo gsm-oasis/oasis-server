565df79d6c967a199c64a6d0813ce514e6ecffc0
shgur7236 <shgurtns7236@naver.com>
2023-01-10 17:24:03 +0900

    :sparkles: HeartService 구현

--- src/main/java/com/real/realoasis/domain/heart/service/HearService.java
+++
@@ -1,4 +0,0 @@
-package com.real.realoasis.domain.heart.service;
-
-public interface HearService {
-}
---
+++ src/main/java/com/real/realoasis/domain/heart/service/HeartService.java
@@ -0,0 +1,8 @@
+package com.real.realoasis.domain.heart.service;
+
+import com.real.realoasis.domain.heart.data.response.HeartResponse;
+
+public interface HeartService {
+
+    HeartResponse getHeart();
+}
