4e16c251ebcdf766d8c5b66a43bdc811c4e4c59b
shgur7236 <shgurtns7236@naver.com>
2023-01-16 15:34:42 +0900

    :recycle: List반환값에서 response객체 자체를 list로 반환

--- src/main/java/com/real/realoasis/domain/questionAnswer/data/response/QuestionAnswerListResponse.java
+++ src/main/java/com/real/realoasis/domain/questionAnswer/data/response/QuestionAnswerListResponse.java
@@ -1,12 +1,14 @@
 package com.real.realoasis.domain.questionAnswer.data.response;
 
+import com.real.realoasis.domain.questionAnswer.data.dto.QuestionAnswerListDto;
 import lombok.*;
+
+import java.util.List;
 
 
 @Getter
 @Builder
 @RequiredArgsConstructor
 public class QuestionAnswerListResponse {
-    private final Long questionId;
-    private final String content;
+    private final List<QuestionAnswerListDto> questions;
 }
