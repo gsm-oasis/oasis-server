203701dee4834040357a075f0b1c9662af521332
shgur7236 <shgurtns7236@naver.com>
2023-01-18 16:24:46 +0900

    :sparkles: 비밀번호 찾기 기능을 비밀번호 재설정기능으로 변경

--- src/main/java/com/real/realoasis/domain/auth/controller/AuthController.java
+++ src/main/java/com/real/realoasis/domain/auth/controller/AuthController.java
@@ -72,7 +72,7 @@ public class AuthController {
     }
 
     // id 를 통해 비밀번호 찾기
-    @PostMapping("/search/pw")
+    @PostMapping("/reset/password")
     public ResponseEntity<SearchPwResponse> searchPW(@RequestBody SearchPwRequest searchPWRequest) {
         SearchPwDto searchPwDto = authConverter.toSearchPwDto(searchPWRequest);
         SearchPwResponse searchPWResponse = authService.searchPW(searchPwDto);
