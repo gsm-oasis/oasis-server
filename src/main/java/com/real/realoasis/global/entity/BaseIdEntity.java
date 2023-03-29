db9d7eb47bc16f0109ff28a7d46cececc819b2ef
shgur7236 <shgurtns7236@naver.com>
2023-03-29 12:14:58 +0900

    :recycle: 기본 idx

---
+++ src/main/java/com/real/realoasis/global/entity/BaseIdEntity.java
@@ -0,0 +1,16 @@
+package com.real.realoasis.global.entity;
+
+import lombok.Getter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseIdEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+}
