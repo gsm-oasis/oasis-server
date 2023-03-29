444b8a1766734fb7ba8bde1f2c2b6bed73612175
shgur7236 <shgurtns7236@naver.com>
2023-03-29 09:32:19 +0900

    :recycle: 1차 jwt 리팩토링

--- src/main/java/com/real/realoasis/global/security/JwtTokenProvider.java
+++ src/main/java/com/real/realoasis/global/security/JwtTokenProvider.java
@@ -1,95 +1,127 @@
 package com.real.realoasis.global.security;
 
+import com.real.realoasis.global.security.authentication.AuthDetailsService;
 import com.real.realoasis.global.security.properties.JwtProperties;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
     private final JwtProperties jwtProperties;
-    private final long ACCESS_TOKEN_EXPIRED_TIME = 2 * 60 * 10000; // 2시간
-    private final long REFRESH_TOKEN_EXPIRED_TIME = 7 * 24 * 60 * 60 * 1000; // 1주
+    private final AuthDetailsService authDetailsService;
+
+    private final long ACCESS_TOKEN_EXPIRED_TIME = 60L * 15;
+    private final long REFRESH_TOKEN_EXPIRED_TIME = 60L * 60 * 24 * 7; // 1주
 
     @AllArgsConstructor
-    enum TokenType{
+    public enum TokenType{
         ACCESS_TOKEN("accessToken"),
         REFRESH_TOKEN("refreshToken");
 
         private final String value;
     }
 
-    private Key getSignInKey(String secretKey) {
-        byte[] keyByte = secretKey.getBytes(StandardCharsets.UTF_8);
-        return Keys.hmacShaKeyFor(keyByte);
+    public String generateAccessToken(String email){
+        return createToken(email,TokenType.ACCESS_TOKEN);
     }
 
-
-    public Claims extractAllClaims(String token) throws ExpiredJwtException, IllegalStateException, UnsupportedOperationException {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey(jwtProperties.getKey()))
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+    public String generateRefreshToken(String email){
+        return createToken(email,TokenType.REFRESH_TOKEN);
     }
 
-    public Long getExpiredTime(String token) {
-        Date expirationTime = extractAllClaims(token).getExpiration();
-        long now = new Date().getTime();
-        return expirationTime.getTime() - now;
-    }
-    // 토큰 생성
-    // userPk == id
-    public String createToken(String userPk, TokenType tokenType, Long expireTime){
-        Claims claims = Jwts.claims().setSubject(userPk); // JWT payload 에 저장되는 정보단위
-        claims.put("tokenType", tokenType.value); // 정보는 key / value 쌍으로 저장
-
-        return Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(new Date(System.currentTimeMillis())) // 토큰 발행 시간 정보
-                .setExpiration(new Date(System.currentTimeMillis() + expireTime)) // 토큰 유효시간 설정
-                .signWith(getSignInKey(jwtProperties.getKey())) // 암호화 알고리즘과 secret 값
-                .compact();
+    public LocalDateTime getAccessTokenExpiredTime(){
+        return LocalDateTime.now().plusSeconds(ACCESS_TOKEN_EXPIRED_TIME);
     }
 
-    public String generateAccessToken(String userPk) {
-        return createToken(userPk, TokenType.ACCESS_TOKEN, ACCESS_TOKEN_EXPIRED_TIME);
+    public LocalDateTime getRefreshTokenExpiredTime(){
+        return LocalDateTime.now().plusSeconds(REFRESH_TOKEN_EXPIRED_TIME);
     }
 
-    public String generateRefreshToken(String userPk) {
-        return createToken(userPk, TokenType.REFRESH_TOKEN, REFRESH_TOKEN_EXPIRED_TIME);
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = authDetailsService.loadUserByUsername(getTokenSubject(token, TokenType.ACCESS_TOKEN));
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
     }
 
-    public String resolveToken(HttpServletRequest req) {
-        String bearerToken = req.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }else{
-            return null;
+    public String resolveToken(HttpServletRequest servletRequest){
+        String token = servletRequest.getHeader("Authorization");
+        if((token != null) && token.startsWith("Bearer ")){
+            return token.replace("Bearer ","");
         }
+        return null;
     }
 
-    // 토큰에서 회원 정보 추출
-    public String getUserPk(String token) {
-        String refreshToken = token.replace("Bearer","");
-        return extractAllClaims(refreshToken).getSubject();
-    }
-
-    // 토큰 유효성, 만료일자 확인
-    public boolean validateToken(String token){
+    public boolean validateToken(String token, TokenType tokenType){
         try {
-            extractAllClaims(token).getExpiration();
+            getTokenBody(token, tokenType).getExpiration();
             return false;
         } catch (Exception e) {
             return true;
+        }
+    }
+
+    public String getTokenSubject(String token, TokenType tokenType){
+        return getTokenBody(token, tokenType).getSubject();
+    }
+
+    public Long getExpiration(String accessToken) {
+        Date expiration = getTokenBody(accessToken, TokenType.ACCESS_TOKEN).getExpiration();
+        long now = new Date().getTime();
+        return (expiration.getTime() - now);
+    }
+
+    private Claims getTokenBody(String token, TokenType tokenType){
+        return Jwts.parserBuilder()
+                .setSigningKey(getKeyByTokenType(tokenType))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getByteKey(String key){
+        byte[] keyByte = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
+
+    private String createToken(String email,TokenType tokenType){
+        return Jwts.builder()
+                .signWith(getKeyByTokenType(tokenType),SignatureAlgorithm.HS256)
+                .claim("email",email)
+                .claim("type",tokenType)
+                .setIssuedAt(new Date())
+                .setSubject(email)
+                .setExpiration(new Date(System.currentTimeMillis() + getTokenExpiredTime(tokenType) * 1000))
+                .compact();
+    }
+
+    private Key getKeyByTokenType(TokenType tokenType){
+        if(tokenType == TokenType.ACCESS_TOKEN){
+            return getByteKey(jwtProperties.getAccessSecret());
+        }
+        else{
+            return getByteKey(jwtProperties.getRefreshSecret());
+        }
+    }
+
+    private long getTokenExpiredTime(TokenType tokenType){
+        if(tokenType == TokenType.ACCESS_TOKEN){
+            return ACCESS_TOKEN_EXPIRED_TIME;
+        }
+        else{
+            return REFRESH_TOKEN_EXPIRED_TIME;
         }
     }
 }
