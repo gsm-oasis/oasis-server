db6045c0cd7016f981e075ce63f993f22bd4b602
shgur7236 <shgurtns7236@naver.com>
2023-06-23 14:52:29 +0900

    :bug: fix

--- src/main/java/com/real/realoasis/domain/couple/domain/repository/CoupleAnniversaryDateRepository.java
+++ src/main/java/com/real/realoasis/domain/couple/domain/repository/CoupleAnniversaryDateRepository.java
@@ -1,6 +1,7 @@
 package com.real.realoasis.domain.couple.domain.repository;
 
+import com.real.realoasis.domain.couple.domain.entity.CoupleAnniversaryDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CoupleAnniversaryDateRepository extends JpaRepository<CoupleAnniversaryDateRepository, Long> {
+public interface CoupleAnniversaryDateRepository extends JpaRepository<CoupleAnniversaryDate, Long> {
 }
