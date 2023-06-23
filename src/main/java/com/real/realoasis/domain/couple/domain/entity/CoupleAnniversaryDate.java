00714b6da8c4f1bc3bbd798086ac026dda410039
shgur7236 <shgurtns7236@naver.com>
2023-06-23 14:48:38 +0900

    :sparkles: 기념일

--- src/main/java/com/real/realoasis/domain/couple/domain/entity/CoupleAnniversaryDate.java
+++ src/main/java/com/real/realoasis/domain/couple/domain/entity/CoupleAnniversaryDate.java
@@ -2,33 +2,30 @@ package com.real.realoasis.domain.couple.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.real.realoasis.global.entity.BaseIdEntity;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AnniversaryDate extends BaseIdEntity {
+@Builder
+@RequiredArgsConstructor
+public class CoupleAnniversaryDate extends BaseIdEntity {
     @Column(nullable = false)
     private long anniversaryDate;
-
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Couple couple;
 
     @Builder
-    public AnniversaryDate(long anniversaryDate, Couple couple) {
+    public CoupleAnniversaryDate(long anniversaryDate, Couple couple) {
         this.anniversaryDate = anniversaryDate;
-        this.couple = couple;
+        this. couple = couple;
     }
 }
--- src/main/java/com/real/realoasis/domain/user/service/Impl/ConnectCoupleServiceImpl.java
+++ src/main/java/com/real/realoasis/domain/user/service/Impl/ConnectCoupleServiceImpl.java
@@ -1,5 +1,8 @@
 package com.real.realoasis.domain.user.service.Impl;
 
+import com.real.realoasis.domain.couple.domain.entity.Couple;
+import com.real.realoasis.domain.couple.domain.repository.CoupleAnniversaryDateRepository;
+import com.real.realoasis.domain.couple.domain.repository.FixedAnniversaryDateRepository;
 import com.real.realoasis.domain.couple.domain.repository.CoupleRepository;
 import com.real.realoasis.domain.user.domain.entity.User;
 import com.real.realoasis.domain.user.domain.repository.UserRepository;
@@ -10,6 +13,7 @@ import com.real.realoasis.domain.user.service.ConnectCoupleService;
 import com.real.realoasis.domain.user.util.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,15 +22,18 @@ public class ConnectCoupleServiceImpl implements ConnectCoupleService {
     private final UserConverter userConverter;
     private final CoupleRepository coupleRepository;
     private final UserRepository userRepository;
+    private final CoupleAnniversaryDateRepository coupleAnniversaryDateRepository;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public ConnectCoupleResDto connectCouple(ConnectCoupleDto connectCoupleDto) {
         User currentUser = userFacade.currentUser();
         User coupleUser = userRepository.findByCoupleCode(connectCoupleDto.getCode());
+        Couple couple = userConverter.toEntity(currentUser, coupleUser);
 
-        coupleRepository.save(userConverter.toEntity(currentUser, coupleUser));
+        coupleRepository.save(couple);
+
         currentUser.updateIsCouple();
-
         return userConverter.toResDto(coupleUser);
     }
 }
