8ffabc487a8059ff488700f7eb44ef804f7ccdd5
shgur7236 <shgurtns7236@naver.com>
2023-06-27 23:56:22 +0900

    :fire: remove file

--- src/main/java/com/real/realoasis/domain/couple/domain/repository/FixedAnniversaryDateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.real.realoasis.domain.couple.domain.repository;
-
-import com.real.realoasis.domain.couple.domain.entity.FixedAnniversaryDate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FixedAnniversaryDateRepository extends JpaRepository<FixedAnniversaryDate, Long> {
-}
