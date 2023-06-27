4a33e3dd7b323595dfa5326ce2657a82e44e1de4
shgur7236 <shgurtns7236@naver.com>
2023-06-27 15:23:47 +0900

    :fire: service 삭제

--- src/main/java/com/real/realoasis/domain/user/service/UpdateAnniversaryTimeService.java
+++
@@ -1,7 +0,0 @@
-package com.real.realoasis.domain.user.service;
-
-import com.real.realoasis.domain.user.presentation.data.dto.AnniversaryTimeChangeDto;
-
-public interface UpdateAnniversaryTimeService {
-    void update(AnniversaryTimeChangeDto anniversaryTimeChangeDto);
-}
