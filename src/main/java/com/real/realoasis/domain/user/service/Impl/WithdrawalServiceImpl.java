2ee4d1e92b47348f4399cf236ffdce9c8b4bf680
shgur7236 <shgurtns7236@naver.com>
2023-07-03 17:40:26 +0900

    :bug: FIX

--- src/main/java/com/real/realoasis/domain/user/service/Impl/WithdrawalServiceImpl.java
+++ src/main/java/com/real/realoasis/domain/user/service/Impl/WithdrawalServiceImpl.java
@@ -1,10 +1,11 @@
 package com.real.realoasis.domain.user.service.Impl;
 
+import com.real.realoasis.domain.couple.domain.entity.Couple;
+import com.real.realoasis.domain.couple.domain.repository.CoupleRepository;
 import com.real.realoasis.domain.user.domain.entity.User;
 import com.real.realoasis.domain.user.domain.repository.UserRepository;
 import com.real.realoasis.domain.user.facade.UserFacade;
 import com.real.realoasis.domain.user.service.WithdrawalService;
-import com.real.realoasis.domain.user.util.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,25 @@ import org.springframework.stereotype.Service;
 public class WithdrawalServiceImpl implements WithdrawalService {
     private final UserFacade userFacade;
     private final UserRepository userRepository;
+    private final CoupleRepository coupleRepository;
 
     @Override
     public void withdrawal() {
-        User user = userFacade.currentUser();
-        userRepository.delete(user);
+        User currentUser = userFacade.currentUser();
+        if(currentUser.getIsCouple()) {
+            Couple couple = currentUser.getCouple();
+            User coupleUser;
+            if(currentUser.equals(couple.getUserA()))
+                coupleUser = couple.getUserB();
+            else
+                coupleUser = couple.getUserA();
+
+            currentUser.unConnectCouple();
+            coupleUser.unConnectCouple();
+            coupleRepository.delete(couple);
+            userRepository.delete(currentUser);
+        }
+        else
+            userRepository.delete(currentUser);
     }
 }
