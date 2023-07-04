fd5f5b6380ba3ff76ff11c17fbb6c986272be1c3
shgur7236 <shgurtns7236@naver.com>
2023-07-05 08:39:10 +0900

    :sparkles: 시간대 설정

--- src/main/java/com/real/realoasis/RealoasisApplication.java
+++ src/main/java/com/real/realoasis/RealoasisApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @ConfigurationPropertiesScan(basePackages = {"com.real.*"})
 @SpringBootApplication
 public class RealoasisApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RealoasisApplication.class, args);
