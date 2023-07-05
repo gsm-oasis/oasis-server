e7d77d54536264b1d1fb6329280ed13fe9220c52
shgur7236 <shgurtns7236@naver.com>
2023-07-05 11:28:50 +0900

    :recycle: 매핑 대폭 수정

--- src/main/java/com/real/realoasis/domain/questionAnswer/domain/entity/QuestionAnswer.java
+++ src/main/java/com/real/realoasis/domain/questionAnswer/domain/entity/QuestionAnswer.java
@@ -1,8 +1,8 @@
 package com.real.realoasis.domain.questionAnswer.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.real.realoasis.domain.couple.domain.entity.Couple;
 import com.real.realoasis.domain.question.domain.entity.Question;
-import com.real.realoasis.domain.user.domain.entity.User;
 import com.real.realoasis.global.entity.BaseIdEntity;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
@@ -16,8 +16,8 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QuestionAnswer extends BaseIdEntity {
-    @Column(nullable = false)
-    private String answer;
+    private String answerA;
+    private String answerB;
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
@@ -25,6 +25,6 @@ public class QuestionAnswer extends BaseIdEntity {
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private User user;
+    private Couple couple;
 
 }
