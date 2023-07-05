dbc8b2479b9cfef2d9b3d62b593a112458ef5341
shgur7236 <shgurtns7236@naver.com>
2023-07-05 10:01:58 +0900

    :sparkles: controller 구현

--- src/main/java/com/real/realoasis/domain/anniversary/presentation/AnniversaryController.java
+++ src/main/java/com/real/realoasis/domain/anniversary/presentation/AnniversaryController.java
@@ -1,24 +1,40 @@
 package com.real.realoasis.domain.anniversary.presentation;
 
+import com.real.realoasis.domain.anniversary.presentation.data.dto.AnniversaryListDto;
+import com.real.realoasis.domain.anniversary.presentation.data.response.AnniversaryListResponse;
 import com.real.realoasis.domain.anniversary.service.AddAnniversaryDateService;
+import com.real.realoasis.domain.anniversary.service.GetAnniversaryListService;
+import com.real.realoasis.domain.anniversary.util.AnniversaryConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/anniversary")
 public class AnniversaryController {
     private final AddAnniversaryDateService addAnniversaryDateService;
+    private final GetAnniversaryListService getAnniversaryListService;
+    private final AnniversaryConverter anniversaryConverter;
 
     // 기념일 추가
     @PostMapping
     public ResponseEntity<Void> addAnniversaryDate(@RequestParam("anniversaryDate") String anniversaryDate) {
         addAnniversaryDateService.add(anniversaryDate);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    // 기념일 목록
+    @GetMapping
+    public ResponseEntity<List<AnniversaryListResponse>> getAnniversaryList() {
+        List<AnniversaryListDto> anniversaryListDtoList = getAnniversaryListService.get();
+        List<AnniversaryListResponse> anniversaryListResponseList = anniversaryListDtoList.stream()
+                .map(anniversaryConverter::toResponse)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(anniversaryListResponseList, HttpStatus.OK);
     }
 }
