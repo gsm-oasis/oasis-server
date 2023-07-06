1a37fff48128a38dca7dbf390d70c3a762040529
shgur7236 <shgurtns7236@naver.com>
2023-07-06 09:08:42 +0900

    :bug: 모든 유저로 변경

--- src/main/java/com/real/realoasis/domain/diary/schedule/DailyScheduler.java
+++ src/main/java/com/real/realoasis/domain/diary/schedule/DailyScheduler.java
@@ -1,19 +1,23 @@
 package com.real.realoasis.domain.diary.schedule;
 
 import com.real.realoasis.domain.user.domain.entity.User;
-import com.real.realoasis.domain.user.facade.UserFacade;
+import com.real.realoasis.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class DailyScheduler {
-    private final UserFacade userFacade;
+    private final UserRepository userRepository;
 
     @Scheduled(cron = "0 0 0 * * *")
     public void executeDailyTask() {
-        User user = userFacade.currentUser();
-        user.resetDailyCount();
+        List<User> userList = userRepository.findAll();
+        for (User user: userList) {
+            user.resetDailyCount();
+        }
     }
 }
