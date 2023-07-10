8835ab9bf539a5e94f0d525f000626439bac93b0
shgur7236 <shgurtns7236@naver.com>
2023-07-10 11:45:07 +0900

    :sparkles: url 설정

--- src/main/java/com/real/realoasis/global/security/SecurityConfig.java
+++ src/main/java/com/real/realoasis/global/security/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
 
                 //anniversary
                 .antMatchers(HttpMethod.POST,"/anniversary").authenticated()
+                .antMatchers(HttpMethod.GET,"/anniversary").authenticated()
 
                 .anyRequest().permitAll();
         http
